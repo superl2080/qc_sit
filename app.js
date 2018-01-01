c6cafa011c33350bbc34d6bbdd9dcc99dc5de04d
Super <SuperL2080@gmail.com>
2018-01-01 22:26:50 +0800

    change env to top

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-var express = require('express');
-var session = require('express-session');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-const MongoStore = require('connect-mongo')(session);
-var test = require('./routes/test');
-var controller = require('./routes/controller');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const controller = require('./routes/controller');
 
-var f_mongo_url;
-if(process.env.NODE_ENV === 'production') {
-    f_mongo_url = 'mongodb://localhost/qingcheng';
-} else {
-    f_mongo_url = 'mongodb://localhost/qingcheng_test';
-}
-mongoose.connect(f_mongo_url, {useMongoClient: true});
+const app = express();
 
-var app = express();
+mongoose.connect(process.env.MONGO_URL, {useMongoClient: true});
+const router = exports.router = express.Router();
 
 app.use(session({
     secret: 'sit51qingchengcom',
@@ -36,17 +29,17 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public/images/favicon.png')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.raw({ type: 'text/xml' }));
+app.use(bodyParser.json({ type: 'application/json' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', test.router);
 app.use('/', controller.router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
@@ -54,12 +47,13 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
     // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+    err.status = err.status || 500;
+    console.error(err);
+    if (res.headersSent) {
+        return next(err);
+    }
     // render the error page
-    res.status(err.status || 500);
-    res.render('frame-error', {title: err.message, message: err.status});
+    res.render('frame-error', { error: err });
 });
 
 module.exports = app;
