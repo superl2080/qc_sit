7d88186e95a1598e1b8de2fb0c3e9352531b22c0
Super <SuperL2080@gmail.com>
2018-01-06 21:03:22 +0800

    finish v1.3

--- api/wechat.js
+++ api/wechat.js
@@ -41,10 +41,10 @@ const PayCreatePrepay = exports.PayCreatePrepay = (param, callback) => {
         spbill_create_ip: param.spbill_create_ip,
         out_trade_no: param.out_trade_no,
         total_fee: param.total_fee,
-        appid = WECHAT_MP_APP_ID;
-        mch_id = WECHAT_PAY_ID;
-        nonce_str = cryptHelper.RandomBytes(16);
-        trade_type = 'JSAPI';
+        appid: WECHAT_MP_APP_ID,
+        mch_id: WECHAT_PAY_ID,
+        nonce_str: cryptHelper.RandomBytes(16),
+        trade_type: 'JSAPI'
     };
     prepayJson.sign = cryptHelper.WechatPaySign(prepayJson);
 
